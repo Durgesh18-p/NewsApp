ed52c955b7602f9198350dc2425ca8dd94605c32
Durgesh <101732303+Durgesh18-p@users.noreply.github.com>
2024-09-27 14:56:43 +0530

    origin fixed

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'http://localhost:5173' || 'https://the-observers-insight.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true 
 }));
