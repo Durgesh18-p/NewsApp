e2328fc2d7459b253709ea84004b4b85b20842b2
Durgesh <101732303+Durgesh18-p@users.noreply.github.com>
2024-09-28 11:56:08 +0530

    js-token installed

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -2,8 +2,6 @@
 export const getTokenFromCookies = () => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; token=`);
-  if (parts.length === 2) {
-    return parts.pop().split(';').shift();
-  }
-  return null;
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null; // or undefined
 };
