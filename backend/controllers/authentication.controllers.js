5b4852add340e2c775a525dcfad1a7ccb0f0f998
Durgesh <101732303+Durgesh18-p@users.noreply.github.com>
2024-09-28 14:02:03 +0530

    admin satus done

--- backend/controllers/authentication.controllers.js
+++ backend/controllers/authentication.controllers.js
@@ -4,18 +4,23 @@ import { Signup } from '../model/authentication.js';
 
 // Secret key for JWT (use environment variable in production)
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key';
+const adminEmails = ["suryadurgesh18@gmail.com"];
 
 // Controller for Signup
 export const userSignup = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    // Check if the user already exists
     const existingUser = await Signup.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
+    let role = "user"; // Default role
 
+    // Check if the email is in the admin list
+    if (adminEmails.includes(email)) {
+        role = "admin"; // Assign admin role if the email matches
+    }
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 12);
 
@@ -51,7 +56,12 @@ export const userLogin = async (req, res) => {
     if (!existingUser) {
       return res.status(404).json({ message: 'User not found' });
     }
+    let role = "user"; // Default role
 
+    // Check if the email is in the admin list
+    if (adminEmails.includes(email)) {
+        role = "admin"; // Assign admin role if the email matches
+    }
     // Compare the password
     const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
     if (!isPasswordCorrect) {
